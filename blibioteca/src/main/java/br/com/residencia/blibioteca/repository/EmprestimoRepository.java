79e19d416460fd0a0a3f3dd70879d22a281d85bb
Lucas <mariolucas.2000@outlook.com>
2022-10-15 15:53:05 -0300

    :sparkles: feat: Dtos

--- blibioteca/src/main/java/br/com/residencia/blibioteca/repository/EmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.residencia.blibioteca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.residencia.blibioteca.entity.Emprestimo;
-
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
-
-}
